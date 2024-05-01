94d47c035a22667ee6548a7f531bee35fbe38848
comet <comet2667@naver.com>
2024-05-01 10:44:07 +0900

    헤더, 메뉴바 애니메이션 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,20 +2,33 @@ import './Header.scss';
 import logo_orange from '../../assets/logo/logo_orange.png';
 import { List } from 'react-bootstrap-icons';
 import { NavLink } from 'react-router-dom';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import NavBar from '../NavBar';
 
 function Header() {
+    const [navCheck, setNavCheck] = useState({
+        check: false,
+        class: 'close',
+        style: {
+            opacity: 1,
+        },
+    });
 
-    const [navCheck, setNavCheck] = useState(true);
+    const onClickNavCheck = ()=>{
+        navCheck.check ? setNavCheck({check: false, class: 'close', style:{opacity: 1}})
+         : setNavCheck({check: true, class: 'open', style:{opacity: 0}});
+    }
 
     return (
         <div className="header__container">
-            <NavBar/>
+            <NavBar navCheck={navCheck} 
+                    setNavCheck={setNavCheck}
+                    onClickNavCheck={onClickNavCheck}
+                    />
             <div className='header__box'>
                 <div className='header__left'>
                     <div className='header__left__menu'>
-                        <List size={40} />
+                        <List size={40} style={navCheck.style} onClick={()=>onClickNavCheck()} />
                     </div>
                     <img src={logo_orange} />
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,28 +3,38 @@ import './NavBar.scss';
 import { useState } from "react";
 import { ArrowLeft } from "react-bootstrap-icons";
 
-function NavBar() {
+function NavBar(props) {
 
     const [checkCommu, setCheckCommu] = useState(false);
+    const [animaiton, setAnimation] = useState('ulClose');
+
+    const onClickComm = ()=>{
+        checkCommu ? setCheckCommu(false) : setCheckCommu(true);
+        checkCommu ? setAnimation('ulClose') : setAnimation('ulOpen');
+    }
+
+    const onClickArrow = ()=>{
+        props.onClickNavCheck()
+        setCheckCommu(false);
+        setAnimation('ulClose');
+    }
 
     return (
-        <div className="nav__container">
+        <div className={props.navCheck.class + ' nav__container'}>
             <div className="nav__header">
-            <h1>메뉴</h1><span><ArrowLeft size={22}/></span>
+            <h1>메뉴</h1><span><ArrowLeft size={22} onClick={()=>onClickArrow()}/></span>
             </div>
             <hr />
             <ul>
                 <li><NavLink to={"/concert"}>공연 정보</NavLink></li>
                 <li><NavLink to={"/artist"}>아티스트 정보</NavLink></li>
                 <li><NavLink to={"/funding"}>아티스트 펀딩</NavLink></li>
-                <li className="nav__item__box" onClick={() => { checkCommu ? setCheckCommu(false) : setCheckCommu(true) }}><div>커뮤니티</div>
-                {checkCommu ?
-                    <ul>
+                <li className="nav__item__box" onClick={() => onClickComm()}><div>커뮤니티</div>
+                    <ul className={animaiton}>
                         <li><NavLink to={"/board/proud"}>아티스트 자랑하기</NavLink></li>
                         <li><NavLink to={"/board/free"}>자유게시판</NavLink></li>
                         <li><NavLink to={"/board/colo"}>콜로세움</NavLink></li>
                     </ul>
-                    : ''}
                 </li>
                 
                 <li><NavLink to={"/notice"}>공지사항</NavLink></li>
