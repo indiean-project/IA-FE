0bea7c015d3c482b67f1ffa900e699ceb9eb9304
leedonghyeob <m715800@naver.com>
2024-05-13 09:59:03 +0900

    adimin.js 수정

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -2,7 +2,7 @@ import API from "../core"
 
 export const adminUser = async() => {
     try {
-        const {data} = await API.post("/api/admin/fundList");
+        const {data} = await API.post("/admin/fundList");
         return data;
     } catch (e){
         console.log(e);
