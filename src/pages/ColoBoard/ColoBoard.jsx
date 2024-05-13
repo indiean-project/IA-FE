d6bea577c122eb00aedfc825d5e1fadfe1871309
ngm0228 <ngm0228@gmail.com>
2024-05-13 22:40:35 +0900

    콜로세움 작업중

--- src/pages/ColoBoard/ColoBoard.jsx
+++ src/pages/ColoBoard/ColoBoard.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom";
 import BoardSidebar from "../../components/BoardSidebar";
 import ColoBar from "../../components/ColoBar";
 import "./ColoBoard.scss";
@@ -8,17 +9,30 @@ function ColoBoard() {
         <div className="coloBoard__container">
             <div className="coloBoard__box">
                 <BoardSidebar />
-                <div className="coloBoard__area">
-                    <div>
-                        <label>콜로세움</label>
+                <div className="coloBoard__items">
+                    <div className="coloBoard__item1">
+                        <div>
+                            <label>콜로세움</label>
+                        </div>
+                        <div>
+                            <select name="" id="">
+                                <option value="boardNo">최신순</option>
+                                <option value="">인기순</option>
+                            </select>
+                        </div>
                     </div>
-                    <div>
-                        <select name="" id="">
-                            <option value="boardNo">최신순</option>
-                            <option value="">인기순</option>
-                        </select>
+                    <div className='coloBoard__item1'>
+                        <div className='coloBoard__category'>커뮤니티 &gt; 콜로세움</div>
+                        <div className='coloBoard__btn'><NavLink to={"/board/enroll"}>글쓰기</NavLink></div>
                     </div>
-                    <div className="coloBoard__items">
+                    <hr />
+                    <div className="coloBoard__items__area">
+                        <div className="coloBoard__item2">
+                            <div>No3469</div>
+                            <div>user01</div>
+                            <div>2024-04-23</div>
+                            <div>아이콘 아이콘</div>
+                        </div>
                         <ColoBar />
                     </div>
                 </div>
