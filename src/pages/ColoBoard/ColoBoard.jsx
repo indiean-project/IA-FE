4a3967ca52ef53c899101692f337035f948b9ddf
ngm0228 <ngm0228@gmail.com>
2024-05-14 11:08:24 +0900

    콜로세움 css

--- src/pages/ColoBoard/ColoBoard.jsx
+++ src/pages/ColoBoard/ColoBoard.jsx
@@ -2,8 +2,16 @@ import { NavLink } from "react-router-dom";
 import BoardSidebar from "../../components/BoardSidebar";
 import ColoBar from "../../components/ColoBar";
 import "./ColoBoard.scss";
+import { MdThumbUp } from "react-icons/md";
+import { BsPencilSquare, BsTrash } from "react-icons/bs";
+import CommonReply from "../../components/CommonReply/CommonReply";
+import { useState } from "react";
 
 function ColoBoard() {
+    const [replyBtn, setReplyBtn] = useState('close');
+    const [replyBtnValue, setReplyBtnValue] = useState('∨');
+
+
     return (
 
         <div className="coloBoard__container">
@@ -28,12 +36,38 @@ function ColoBoard() {
                     <hr />
                     <div className="coloBoard__items__area">
                         <div className="coloBoard__item2">
-                            <div>No3469</div>
+                            <div>No.3469</div>
                             <div>user01</div>
-                            <div>2024-04-23</div>
-                            <div>아이콘 아이콘</div>
+                            <div>2024-04-23 <BsPencilSquare /> <BsTrash /></div>
                         </div>
                         <ColoBar />
+                        <p className="coloBoard__content">
+                            본문 내용@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+                        </p>
+                        <div className="coloBoard__item3">
+                            <div className="coloBoard__like">
+                                <div><MdThumbUp /></div>
+                                <div>38</div>
+                            </div>
+                        </div>
+
+                        <div className="coloBoard__item4">
+
+                            <div className="coloBoard__reply__btn" onClick={() => {
+                                setReplyBtn(() => {
+                                    replyBtn === 'close' ? setReplyBtn('open') : setReplyBtn('close')
+                                }),
+                                    setReplyBtnValue(() => {
+                                        replyBtn === 'close' ? setReplyBtnValue('∧') : setReplyBtnValue('∨')
+                                    })
+                            }}>{replyBtnValue} 댓글(38)</div>
+
+                            <div className="coloBoard__report__btn">신고</div>
+                        </div>
+                        <div className={replyBtn === 'close' ? 'coloBoard__reply__close' : ''}>
+                            <CommonReply />
+
+                        </div>
                     </div>
                 </div>
             </div>
