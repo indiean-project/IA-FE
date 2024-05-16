c4356d32972f8c794ef092c0b28b9d2cc275a5e4
ngm0228 <ngm0228@gmail.com>
2024-05-16 17:14:19 +0900

    페이지네이션 추가 및 오류수정

--- src/pages/ColoBoard/ColoBoard.jsx
+++ src/pages/ColoBoard/ColoBoard.jsx
@@ -7,25 +7,32 @@ import { BsPencilSquare, BsTrash } from "react-icons/bs";
 import CommonReply from "../../components/CommonReply/CommonReply";
 import { useEffect, useState } from "react";
 import { pageMove } from "../../apis/pagination";
+import { useRecoilState } from "recoil";
+import { cPage } from "../../recoil/page";
+import PaginationBar from "../../components/PaginationBar";
 
 function ColoBoard() {
     const [replyBtn, setReplyBtn] = useState([]);
     const [boardList, setBoardList] = useState([]);
     const [sort, setSort] = useState("boardNo");
     const url = "board/colo/boardlist";
-    const page = 1;
+    const [keyword, setKeyword] = useState("");
+    const [currentPage, setCurrentPage] = useRecoilState(cPage);
+    const [pageInfo, setPageInfo] = useState();
 
+    async function list() {
+        const list = await pageMove({
+            url: url,
+            page: currentPage,
+            sort: sort,
+            keyword: keyword
+        });
+        console.log(list);
+        setBoardList(list.listDto);
+        setPageInfo(list.pageinfo);
+        setReplyBtn(new Array(list.length).fill('close'));
+    }
     useEffect(() => {
-        async function list() {
-            const list = await pageMove({
-                url: url,
-                page: page,
-                sort: sort
-            });
-            console.log(list);
-            setBoardList(list);
-            setReplyBtn(new Array(list.length).fill('close'));
-        }
         list();
     }, [])
 
@@ -62,37 +69,37 @@ function ColoBoard() {
                     <hr />
                     {boardList != undefined && boardList.map((item, index) => {
                         return (
-                            <>
-                                <div className="coloBoard__items__area" key={index}>
-                                    <div className="coloBoard__item2">
-                                        <div>No.{item.boardNo}</div>
-                                        <div>{item.nickname}</div>
-                                        <div>{item.enrollDate} <BsPencilSquare /> <BsTrash /></div>
-                                    </div>
-                                    <ColoBar list={item} />
-                                    <p className="coloBoard__content" dangerouslySetInnerHTML={{ __html: item.boardContent }}></p>
-                                    <div className="coloBoard__item3">
-                                        <div className="coloBoard__like">
-                                            <div><MdThumbUp /></div>
-                                            <div>{item.likeCount}</div>
-                                        </div>
-                                    </div>
-
-                                    <div className="coloBoard__item4">
-
-                                        <div className="coloBoard__reply__btn" onClick={() => toggleReplyBtn(index)}>
-                                            {replyBtn[index] === 'close' ? '∧' : '∨'} 댓글({item.replies})</div>
-
-                                        <div className="coloBoard__report__btn">신고</div>
-                                    </div>
-                                    <div className={replyBtn[index] === 'close' ? 'displayNone' : ''}>
-                                        <CommonReply />
-
+                            <div className="coloBoard__items__area" key={index}>
+                                <div className="coloBoard__item2">
+                                    <div>No.{item.boardNo}</div>
+                                    <div>{item.nickname}</div>
+                                    <div>{item.enrollDate} <BsPencilSquare /> <BsTrash /></div>
+                                </div>
+                                <ColoBar list={item} />
+                                <p className="coloBoard__content" dangerouslySetInnerHTML={{ __html: item.boardContent }}></p>
+                                <div className="coloBoard__item3">
+                                    <div className="coloBoard__like">
+                                        <div><MdThumbUp /></div>
+                                        <div>{item.likeCount}</div>
                                     </div>
                                 </div>
-                            </>
+
+                                <div className="coloBoard__item4">
+
+                                    <div className="coloBoard__reply__btn" onClick={() => toggleReplyBtn(index)}>
+                                        {replyBtn[index] === 'close' ? '∧' : '∨'} 댓글({item.replies})</div>
+
+                                    <div className="coloBoard__report__btn">신고</div>
+                                </div>
+                                <div className={replyBtn[index] === 'close' ? 'displayNone' : ''}>
+                                    <CommonReply />
+
+                                </div>
+                            </div>
+
                         )
                     })}
+                    <PaginationBar pageInfo={pageInfo} list={list} />
                 </div>
             </div>
         </div>
