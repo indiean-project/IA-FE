928ae2d67ae8123bb562e592430bc1822afa3c71
leedonghyeob <m715800@naver.com>
2024-05-17 12:36:47 +0900

    임시 커밋

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -1,5 +1,8 @@
 import { API } from "../core"
 
+ 
+//******* 펀드 승급요청 관련 api**********// 
+// 펀딩 관련 리스트 통신 api
 export const adminFund = async() => {
     try {
         const {data} = await API.post("api/admin/fundList");
@@ -10,9 +13,33 @@ export const adminFund = async() => {
 
 }
 
+// 펀딩 관련 검색 api
 export const adminFundSearch = async(params) => {
     try {
         const {data} = await API.post("api/admin/fundSearch", params);
+        return data;
+    } catch (e){
+        console.log(e);
+    }
+}
+
+
+//******* 문의사항  관련 api**********// 
+// 문의사항 관련 리스트 통신 api
+export const adminQuestion = async() => {
+    try {
+        const {data} = await API.post("api/admin/question");
+        return data;
+    } catch (e){
+        console.log(e);
+    }
+
+}
+
+ // 문의사항 관련 검색api
+export const adminQuestionSearch = async(params) => {
+    try {
+        const {data} = await API.post("api/admin/questionSection", params);
         return data;
     } catch (e){
         console.log(e);
--- src/components/AdminQuestionSeciton/AdminQuestionSection.jsx
+++ src/components/AdminQuestionSeciton/AdminQuestionSection.jsx
@@ -1,35 +1,87 @@
-import AdminFundingApproval from "../AdminFundingApproval";
+import { useEffect, useState } from 'react';
+import { adminQuestion, adminQuestionSearch } from "../../apis/admin";
 
 
-function AdminQuestionSection(){
+function AdminQuestionSection() {
 
-    return(
+    const [questionRequest, setQuestionRequest] = useState([])
+
+    // const [searchQuestion, setSearchQuestion] = usestate({
+    //         keyword: '',
+    //         type: '',
+    // });
+
+    
+    useEffect(() => {
+        getList();
+    }, []);
+
+    const getList = async () => {
+        const result = await adminQuestion();
+        console.log(result);
+        if (result.status === "SUCCESS")
+            setQuestionRequest(result.data);
+    }
+
+
+    const onClickSearch= async()=>{
+        console.log(searchQuestion);
+        const result = await adminQuestionSearch(searchQuestion);
+        setQuestionRequest(result.data);
+        console.result(result);
+    }
+
+
+
+    const onChangeSearch = (e)=>{
+        setSearchQuestion({
+            ...searchQuestion,
+            [e.target.name]: e.target.value
+        })
+
+    }
+    return (
         <div>
-           
-            <br/><br/><br/><br/>
-            <h1>문의사항 리스트</h1>
-            <select>
-                <option>작성자</option>
-                <option>제목</option>
-                <option>내용</option>
-            </select>
-            <input type="text" name="keyowrd" value=""/>
-            <button type="submit">검색</button>
-            <table>
-                <thead>
-                    <tr>
-                        <th>No.</th>                      
-                        <th>제목</th>
-                        <th>내용</th>
-                        <th>작성자</th>
-                    </tr>                    
-                </thead>
-                <tbody>
-                    <tr>
-                        
-                    </tr>
-                </tbody>
-            </table>
+            <div className="adiminquestion__container">
+
+                <h1 className="adminquestion___request">문의 사항</h1>
+                <div className="adminquestion__optionsearch">
+                 
+                    <select name='type' onChange={(e) => onChangeSearch(e)}>
+                        <option value={'1'}>문의자</option>
+                        <option value={'2'}>문의일</option>
+                        <option value={'3'}>답변 여부</option>
+                    </select>
+                    
+                    <input type="" name="" />
+                    {/* <input type="text" name="keyword" value={searchQuestion.keyword} onChange={(e) => onChangeSearch(e)} /> */}
+                    <button onClick={() => onClickSearch()}>검색</button>
+                   
+                </div>
+                <table className="adminquestion__table" border={1}>
+                    <thead className="adminquestion__tablehead">
+                        <tr>
+                            <th>No.</th>
+                            <th>문의내용</th>
+                            <th>문의자</th>
+                            <th>문의일</th>
+                            <th>답변여부</th>
+                        </tr>
+                    </thead>
+                    <tbody className="adminquestion__tablebody">
+                    
+                        {questionRequest.map((data, index) => (
+                            <tr key={index}>
+                                <td>{data.questionNo}</td>
+                                <td>{data.qContent}</td>
+                                <td>{data.userNo}</td>
+                                <td>{data.qDate}</td>
+                                <td>{data.ansYn}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 
