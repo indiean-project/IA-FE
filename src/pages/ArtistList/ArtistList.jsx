71efa36e20e77c4023404c8799ad00cccb970951
TaeGeon <taegeon9358@naver.com>
2024-05-20 00:06:00 +0900

    2024-05-19 작업

--- src/pages/ArtistList/ArtistList.jsx
+++ src/pages/ArtistList/ArtistList.jsx
@@ -1,33 +1,34 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './ArtistList.scss'
-import { useRecoilState } from 'recoil';
-import { cPage } from '../../recoil/page';
 import { NavLink } from 'react-router-dom';
 import ArtistItem from '../../components/ArtistItem';
 import { PuffLoader } from 'react-spinners';
 import { useInView } from 'react-intersection-observer';
 import {selectArtist} from '../../apis/artist/artist';
 
+
 function ArtistList() {
     const [ref, inView] = useInView();
+    const [keyword,setKeyword] = useState('');
     const [artistList, setArtistList] = useState([]);
-    const [sort, setSort] = useState("createDate");
-    const [keyword, setKeyword] = useState("");
-    const [currentPage,setCurrentPage] = useRecoilState(cPage);
+    const [selectItem, setSelectItem] = useState({
+        keyword :keyword,
+        sort : 'createDate',
+        size : 10
+    })
     const [loadingCheck, setLoadingCheck] = useState(false);
     const handleKeyEnter = (e) => {
         if (e.key === 'Enter') {
-            list();
+            setSelectItem({
+                ...selectItem,
+                keyword : keyword
+            })
         }
     }
     const list = async () => {
 
-        const result = await selectArtist({
-            page: currentPage,
-            sort: sort,
-            keyword: keyword
-        });
-        setArtistList(result.listDto)
+        const result = await selectArtist(selectItem);
+        setArtistList(result)
     }
     useEffect(()=>{
         if(inView){
@@ -35,12 +36,15 @@ function ArtistList() {
             setTimeout(()=>{
                 setSelectItem({
                     ...selectItem,
-                    page: selectItem.page + 10
+                    size: selectItem.size + 10
                 })
                 setLoadingCheck(false);
             }, 1000)
         }
     },[inView])
+    useEffect(()=>{
+        list();
+    },[selectItem])
 
 
     return (
@@ -48,11 +52,11 @@ function ArtistList() {
             <div className='artist__list__container'>
                 <div className='title'>
                     <h1>ARTIST LIST</h1>
-                    <input className='artist__search' type="text" name="keyword" placeholder='검색어를 입력하세요' value={keyword} onChange={(e) => { setKeyword(e.target.value) }} onKeyPress={(e) => { handleKeyEnter(e) }} />
+                    <input className='artist__search' type="text" name="keyword" value={keyword} placeholder='검색어를 입력하세요' onChange={(e)=>{setKeyword(e.target.value)}} onKeyPress={(e) => { handleKeyEnter(e) }} />
                 </div>
                 <div className='btn__area'>
                     <div>
-                        <select name="sort" id="select-id" value={sort} onChange={(e) => { setSort(e.target.value), setCurrentPage(1) }}>
+                        <select name="sort" id="select-id" value={selectItem.sort}>
                             <option value="createDate">최신순</option>
                             <option value="debut">데뷔</option>
                         </select> &nbsp;
@@ -65,7 +69,7 @@ function ArtistList() {
                 </div>
                 <div className='spinner__box'>
                     <div className='spinner' ref={ref}>
-                            {loadingCheck ? <PuffLoader color='#F2613F'/> : '모든 펀딩을 불러왔습니다.' }
+                            {loadingCheck ? <PuffLoader color='#F2613F'/> : '모든 아티스트를 불러왔습니다.' }
                     </div>
                 </div>
             </div>
