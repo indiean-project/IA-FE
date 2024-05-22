8639f5c0849c0ef10d47a8d52f542788e49f820a
comet <comet2667@naver.com>
2024-05-23 00:31:05 +0900

    에러페이지 처리 추가

--- src/main.jsx
+++ src/main.jsx
@@ -113,6 +113,10 @@ const router = createBrowserRouter([
       }
     ],
   },
+  {
+    path: '/error',
+    element: <ErrorPage/>
+  }
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
