9676d718fd93911e1ddf5716e838564eee57d6c8
leedonghyeob <m715800@naver.com>
2024-05-28 15:07:19 +0900

    fundingList 불러오기 구현 완료

--- src/components/AdminFundingApproval/AdminFundingApproval.jsx
+++ src/components/AdminFundingApproval/AdminFundingApproval.jsx
@@ -16,79 +16,16 @@ function AdminFundingApproval() {
     // 아래 주석 처리한 함수들은 backEnd 작업 후 활성화 하시면 됩니다.
     // 단, test에 기재해놓은 데이터 형식에 맞춰 dto 반환해주셔야 합니다.
 
-    // useEffect(()=> {
-    //     getList();
-    // }, []);
+    useEffect(()=> {
+        getList();
+    }, []);
 
-    // const getList = async () => {
-    //     const result = await adminUser();
-    //     console.log(result);
-    //     if (result.status === "SUCCESS")
-    //         setFundingRequest(result.data);
-    // }
-
-    const test = [
-        {
-            fundNo: 1,
-            artistName: "박혜성밴드",
-            fundTitle: "박혜성 점심 사주기 프로젝트",
-            createDate: '2024-05-23',
-            startDate: '2024-05-30',
-            endDate: '2024-06-25',
-            responseDate: '', //컬럼 추가함 -> BACKEND쪽에서 당일 날짜로 update 필요
-            fundStatus: 'AWAIT' //여유가 있다면 해당 문구를 '승인대기' 등과 같은 value 값으로 변환해서 뽑아주세요(BackEnd)
-        },
-        {
-            fundNo: 2,
-            artistName: "박혜성밴드",
-            fundTitle: "박혜성 점심 사주기 프로젝트",
-            createDate: '2024-05-23',
-            startDate: '2024-05-30',
-            endDate: '2024-06-25',
-            responseDate: '', //컬럼 추가함
-            fundStatus: 'AWAIT'
-        },
-        {
-            fundNo: 3,
-            artistName: "박혜성밴드",
-            fundTitle: "박혜성 점심 사주기 프로젝트",
-            createDate: '2024-05-23',
-            startDate: '2024-05-30',
-            endDate: '2024-06-25',
-            responseDate: '', //컬럼 추가함
-            fundStatus: 'AWAIT'
-        },
-        {
-            fundNo: 4,
-            artistName: "박혜성밴드",
-            fundTitle: "박혜성 점심 사주기 프로젝트",
-            createDate: '2024-05-23',
-            startDate: '2024-05-30',
-            endDate: '2024-06-25',
-            responseDate: '', //컬럼 추가함
-            fundStatus: 'AWAIT'
-        },
-        {
-            fundNo: 5,
-            artistName: "박혜성밴드",
-            fundTitle: "박혜성 점심 사주기 프로젝트",
-            createDate: '2024-05-23',
-            startDate: '2024-05-30',
-            endDate: '2024-06-25',
-            responseDate: '', //컬럼 추가함
-            fundStatus: 'AWAIT'
-        },
-        {
-            fundNo: 6,
-            artistName: "박혜성밴드",
-            fundTitle: "박혜성 점심 사주기 프로젝트",
-            createDate: '2024-05-23',
-            startDate: '2024-05-30',
-            endDate: '2024-06-25',
-            responseDate: '', //컬럼 추가함
-            fundStatus: 'AWAIT'
-        },
-    ]
+    const getList = async () => {
+        const result = await adminUser();
+        console.log(result);
+        if (result.status === "SUCCESS")
+            setFundingRequest(result.data);
+    } 
 
     const searchCategory = [
         { label: '전체', value: 'all' },
@@ -125,17 +62,17 @@ function AdminFundingApproval() {
     }
 
     const onClickManageBtn = async (fundNo, fundStatus) => {
-        //backEnd 작업 후 활성화 시켜주세요.
+     
 
-        // const result = await updateFundStatus({
-        //     fundNo: fundNo,
-        //     fundStatus: fundStatus
-        // })
-        // if (result.status === 'SUCCESS'){
-        //     toast.success('처리가 완료되었습니다.');
-        // } else {
-        //     toast.error('처리 실패');
-        // }
+        const result = await updateFundStatus({
+            fundNo: fundNo,
+            fundStatus: fundStatus
+        })
+        if (result.status === 'SUCCESS'){
+            toast.success('처리가 완료되었습니다.');
+        } else {
+            toast.error('처리 실패');
+        }
     }
 
     const onClickSearch = async() => {
@@ -188,7 +125,7 @@ function AdminFundingApproval() {
                     </tr>
                 </thead>
                 <tbody>
-                    {test.map((item) => {
+                    {fundingRequest.map((item) => {
                         return (
                             <tr key={item.fundNo}>
                                 <td>{item.fundNo}</td>
