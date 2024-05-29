106468f9923c98eb4ec70f2ec65ade12a70f9dfc
comet <comet2667@naver.com>
2024-05-29 16:16:06 +0900

    이미지 업로드 오류 수정

--- src/core/ScrollTop.jsx
+++ src/core/ScrollTop.jsx
@@ -1,10 +1,20 @@
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom"
+import { useRecoilValue } from "recoil";
+import { tempImgState } from "../recoil/tempImgStorage";
+import { imgDelete } from "../apis/imgFilter";
 
 export const ScrollTop = () => {
     const { pathname } = useLocation();
+    const tempImg = useRecoilValue(tempImgState);
 
     useEffect(()=>{
         window.scrollTo(0, 0);
+        let list = new Array();
+        tempImg.bossImg.filter((item)=> item != '').forEach((item)=>{
+            list.push(item);
+        });
+        list.length != 0 && imgDelete(list);
+        tempImg.contentImg.length != 0 && imgDelete(tempImg.contentImg);
     }, [pathname])
 }
