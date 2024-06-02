6d4d662c4624ae4dd9bf56bd9c1d5cfb994e3ce2
comet <comet2667@naver.com>
2024-06-02 21:49:08 +0900

    오타 수정

--- src/pages/ArtistEnrollForm/ArtistEnrollForm.jsx
+++ src/pages/ArtistEnrollForm/ArtistEnrollForm.jsx
@@ -181,7 +181,7 @@ function ArtistEnrollForm() {
                             return (
                                 <>
                                     {img != '' ?
-                                        <div className='artistEnrollForm__img__input' onClick={() => imageDelete(img, idx)} key={inx}>
+                                        <div className='artistEnrollForm__img__input' onClick={() => imageDelete(img, idx)} key={idx}>
                                             <img src={img} />
                                             <div className='delete__icon'>
                                                 <div className='delete__background'></div>
